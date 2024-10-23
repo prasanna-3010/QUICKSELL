a0bd4bb31bc5cef5fa31b87f9e243d982386e7be
Prasanna Venkatesh M <136181078+prasanna-3010@users.noreply.github.com>
2024-10-23 14:52:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg'; // Ensure this file exists in the src folder
+import './App.css'; // Ensure this file exists in the src folder
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
